47224219b678062cfad2ce0a388e815905b8e7f6
BrunoBarlaan <brunobarlaan@gmai.com>
2022-08-19 19:29:28 -0300

    Criando classes

---
+++ Usuario.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TCC
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public int Idade { get; set; }
+        public string Endereco { get; set; }
+    }
+}
